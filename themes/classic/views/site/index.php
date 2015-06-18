02fdc40d0cfe87c3908a71af55e991a8150655a8
audiua <audiua@yandex.ru>
2015-06-18 13:16:37 +0300

    remove

--- themes/classic/views/site/index.php
+++ themes/classic/views/site/index.php
@@ -1,6 +1,6 @@
 <div id="fader">
-    <img width="705" height="256" src="http://mlombard.com.ua/userfiles/image/banners/new/1.jpg" alt="" style="display: block; "></img>
-    <img width="705" height="256" src="http://mlombard.com.ua/userfiles/image/banners/new/2.jpg" alt="" style="display: block; "></img>
-    <img width="705" height="256" src="http://mlombard.com.ua/userfiles/image/banners/new/3.jpg" alt="" style="display: none;"></img>
+    <img width="705" height="256" src="http://mlombard.com.ua/userfiles/image/banners/new/1.jpg" alt="" style="display: block; "/>
+    <img width="705" height="256" src="http://mlombard.com.ua/userfiles/image/banners/new/2.jpg" alt="" style="display: block; "/>
+
 </div>
 <?php echo $model->content; ?>
