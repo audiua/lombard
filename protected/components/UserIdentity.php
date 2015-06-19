bec74796bb1b4f298b7096ee9f460a4bc17354af
audiua <audiua@yandex.ru>
2015-06-19 13:56:18 +0300

    fix

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -20,7 +20,7 @@ class UserIdentity extends CUserIdentity
 		$users=array(
 			// username => password
 			// 'demo'=>'demo',
-			'inside_admin'=>'admin_123456',
+			'admin'=>'admin_123456',
 		);
 		if(!isset($users[$this->username]))
 			$this->errorCode=self::ERROR_USERNAME_INVALID;
