a5efba4750424ecee7ff4094f8248938102be211
audiua <audiua@yandex.ru>
2015-06-21 15:58:44 +0300

    fix

--- themes/classic/views/site/index.php
+++ themes/classic/views/site/index.php
@@ -1,6 +1,6 @@
 <div id="fader">
-    <img width="705" height="256" src="http://mlombard.com.ua/userfiles/image/banners/new/1.jpg" alt="" style="display: block; "/>
-    <img width="705" height="256" src="http://mlombard.com.ua/userfiles/image/banners/new/2.jpg" alt="" style="display: block; "/>
+    <img width="705" height="256" src="http://lombard-econom.pp.ua/img/1.jpg" alt="" style="display: block; "/>
+    <img width="705" height="256" src="http://lombard-econom.pp.ua/img/2.jpg" alt="" style="display: block; "/>
 
 </div>
 <?php echo $model->content; ?>
